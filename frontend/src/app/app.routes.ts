1b9d9fdd1c055c79568aca2ccef1a959fad28807
Abbas285 <abbasali0698@outlook.com>
2025-08-23 22:04:05 +0500

    added table and also create aservice for getting catigory list

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { CategoryComponent } from './components/manage/category/category.component';
-import { CategoryFormComponent } from './components/category-form/category-form.component';
+import { CategoryFormComponent } from './components/manage/category-form/category-form.component';
 
 export const routes: Routes = [
   {
--- frontend/src/app/components/manage/category/category.component.ts
+++ frontend/src/app/components/manage/category/category.component.ts
@@ -1,11 +1,48 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { AfterViewInit, ViewChild, Component } from '@angular/core';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatSort, MatSortModule } from '@angular/material/sort';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-category',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatTableModule,
+    MatSortModule,
+    MatPaginatorModule,
+  ],
+
   templateUrl: './category.component.html',
   styleUrl: './category.component.scss',
 })
-export class CategoryComponent {}
+export class CategoryComponent {
+  displayedColumns: string[] = ['id', 'name', 'progress', 'fruit', 'action'];
+  dataSource: MatTableDataSource<any>;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  constructor() {
+    // Example mock data
+
+    this.dataSource = new MatTableDataSource([] as any);
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+}
