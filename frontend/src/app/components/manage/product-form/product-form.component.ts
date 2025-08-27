4c339b4cac368725fa3b45dcef4189bb36938e29
Abbas285 <abbasali0698@outlook.com>
2025-08-28 00:53:46 +0500

    update

--- frontend/src/app/components/manage/product-form/product-form.component.ts
+++ frontend/src/app/components/manage/product-form/product-form.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  ReactiveFormsModule,
+  Validators,
+  FormsModule,
+} from '@angular/forms';
+import { Component, inject } from '@angular/core';
+import { MatFormField } from '@angular/material/form-field';
+import { MatButton } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-form',
   standalone: true,
-  imports: [],
+
+  imports: [
+    ReactiveFormsModule,
+    MatButton,
+    MatFormField,
+    MatInputModule,
+    FormsModule,
+  ],
   templateUrl: './product-form.component.html',
-  styleUrl: './product-form.component.scss'
+  styleUrl: './product-form.component.scss',
 })
 export class ProductFormComponent {
+  formModule = inject(FormBuilder);
 
+  productForm = this.formModule.group({
+    name: [null, [Validators.required, Validators.minLength(5)]],
+    shortDescription: [null, [Validators.required, Validators.minLength(10)]],
+    description: [null, [Validators.required, Validators.minLength(50)]],
+    price: [null, [Validators.required]],
+    discount: [],
+    images: [],
+    categoryId: [null, [Validators.required]],
+  });
 }
