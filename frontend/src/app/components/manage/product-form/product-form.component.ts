d6168537b471b9430377e2c2fdf86da5f53f9d8e
Abbas285 <abbasali0698@outlook.com>
2025-09-03 01:46:50 +0500

    update

--- frontend/src/app/components/manage/product-form/product-form.component.ts
+++ frontend/src/app/components/manage/product-form/product-form.component.ts
@@ -1,41 +1,47 @@
+import { Component, inject } from '@angular/core';
 import {
   FormBuilder,
-  ReactiveFormsModule,
   Validators,
-  FormsModule,
+  FormArray,
+  ReactiveFormsModule,
 } from '@angular/forms';
-import { Component, inject } from '@angular/core';
 import { MatFormField } from '@angular/material/form-field';
-import { MatButton } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
-import { ActivatedRoute, Router } from '@angular/router';
+import { MatButton } from '@angular/material/button';
 
 @Component({
   selector: 'app-product-form',
   standalone: true,
-
-  imports: [
-    ReactiveFormsModule,
-    MatButton,
-    MatFormField,
-    MatInputModule,
-    FormsModule,
-  ],
+  imports: [ReactiveFormsModule, MatFormField, MatInputModule, MatButton],
   templateUrl: './product-form.component.html',
-  styleUrl: './product-form.component.scss',
+  styleUrls: ['./product-form.component.scss'],
 })
 export class ProductFormComponent {
-  formModule = inject(FormBuilder);
+  fb = inject(FormBuilder);
 
-  productForm = this.formModule.group({
-    name: [null, [Validators.required, Validators.minLength(5)]],
-    shortDescription: [null, [Validators.required, Validators.minLength(10)]],
-    description: [null, [Validators.required, Validators.minLength(50)]],
+  productForm = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(5)]],
+    shortDescription: ['', [Validators.required, Validators.minLength(10)]],
+    description: ['', [Validators.required, Validators.minLength(50)]],
     price: [null, [Validators.required]],
     discount: [],
-    images: [],
-    categoryId: [null, [Validators.required]],
+    images: this.fb.array([]), // FormArray
+    categoryId: ['', [Validators.required]],
   });
+
+  get images(): FormArray {
+    return this.productForm.get('images') as FormArray;
+  }
+  ngOnInit() {
+    this.addImage();
+  }
+  addImage(value: string = '') {
+    this.images.push(this.fb.control(value, Validators.required));
+  }
+
+  removeImage(index: number) {
+    this.images.removeAt(index);
+  }
 
   addProduct() {
     console.log(this.productForm.value);
